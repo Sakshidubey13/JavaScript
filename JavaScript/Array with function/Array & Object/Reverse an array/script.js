51af61acc3b2a35f0357f98b57dc11b7473ed692
Dubey Sakshi <sakshidubey1304@gmail.com>
2026-02-18 17:03:13 +0530

    Add files via upload

---
+++ JavaScript/Array with function/Array & Object/Reverse an array/script.js
@@ -0,0 +1,10 @@
+// 1. Reverse an array Write a function to reverse the array [1, 2, 3, 4, 5] ✅ Expected Output: [5, 4, 3, 2, 1]
+
+const num = [1, 2, 3, 4, 5];
+
+
+function reverseArray(arr) {
+    return arr.reverse();
+
+}
+console.log(reverseArray(num));
