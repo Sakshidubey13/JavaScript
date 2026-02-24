a4287875b11ceb3e4a531c7e700508030472a997
Dubey Sakshi <sakshidubey1304@gmail.com>
2026-02-24 16:08:30 +0530

    Add files via upload

---
+++ JavaScript/Condition/Findmaximumfor5_number/script.js
@@ -0,0 +1,137 @@
+//find maximum for 5 number
+
+const digit = a = 20, b = 56, c= 78,d=83,e=100;
+
+if(a>b){
+    if(a>c){
+        if(a>d)
+        {    
+            if(a>e)
+            {
+              console.log("A is maximum !");
+            }else{
+              console.log("E is maximum !");
+            }
+        }
+        else{
+            if(d>e){
+                console.log("D is maximum !");
+            }
+            else{
+                console.log("E is maximum !");
+            
+            }   
+        }
+    }
+    else{
+        if(c>d){
+            if(c>e){
+                console.log("C is maximum !");
+            }
+            else{
+                console.log("E is maximum !");
+                
+            }
+
+        }
+        else{
+            if(b>c)
+            {
+            
+                if(b>d)
+                {
+                     if(b>e)
+                     {
+                       console.log("B is maximum !");
+                     }
+                     else{
+
+                        console.log("E is maximum !");
+                     }
+                }
+                else{
+                if(d>e){
+                    console.log("D id maximum !");
+                }
+                  else{
+                      console.log("E id maximum !");
+                  }
+                    
+                }
+            }
+            else{
+                if(c>d){
+                   if(c>e){
+                    console.log("C is maximum !");
+                   }
+                   else{
+                    console.log("E is maximum !");
+                   }
+                }
+                else{
+                    if(d>e){
+                        console.log("D is maximum !");
+
+                    }
+                    else{
+                          console.log("E id maximum !");
+                    }
+
+                }
+
+            }
+
+
+        }
+
+    }
+
+}
+else{
+     if(b>c)
+            {
+            
+                if(b>d)
+                {
+                     if(b>e)
+                     {
+                       console.log("B is maximum !");
+                     }
+                     else{
+
+                        console.log("E is maximum !");
+                     }
+                }
+                else{
+                if(d>e){
+                    console.log("D id maximum !");
+                }
+                  else{
+                      console.log("E id maximum !");
+                  }
+                    
+                }
+            }
+            else{
+                if(c>d){
+                   if(c>e){
+                    console.log("C is maximum !");
+                   }
+                   else{
+                    console.log("E is maximum !");
+                   }
+                }
+                else{
+                    if(d>e){
+                        console.log("D is maximum !");
+
+                    }
+                    else{
+                          console.log("E id maximum !");
+                    }
+
+                }
+
+            }
+
+}
